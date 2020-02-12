3a2a503c414a11a4fbb34ccda5dac3f9012408a0
functionxu123 <1229853312@qq.com>
2020-02-12 23:00:17 +0800

    sdf

--- Server/tools/Config.py
+++ Server/tools/Config.py
@@ -5,9 +5,8 @@ Created on 2020年2月12日
 @author: sherl
 '''
 Port=7999   #监听端口
-Timeout=5   #recv的超时时间
-HeartBeat=3 #心跳时间间隔
+Timeout=5   #recv的超时时间 s
+HeartBeat=3 #心跳时间间隔 s 
+BuffSize=1024   #recv bufsize
 
 
-if __name__ == '__main__':
-    pass
